6e28aed4d2c1a01f7a59c7da1b1e0727403d47a6
Reducks <reducks3000@gmail.com>
2019-10-15 06:12:14 -0500

    added scripts to the index and added credentials to script.js for firebase

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,20 @@
-   
+    // Initialize Firebase
+    const firebaseConfig = {
+      apiKey: "AIzaSyAGfO3B6dYL7iwUzej_0fYle9Fb9sbt_Qg",
+      authDomain: "traintime-1d8bf.firebaseapp.com",
+      databaseURL: "https://traintime-1d8bf.firebaseio.com",
+      projectId: "traintime-1d8bf",
+      storageBucket: "traintime-1d8bf.appspot.com",
+      messagingSenderId: "142390850680",
+      appId: "1:142390850680:web:d4c0431723cd9ccb74698d",
+      measurementId: "G-GP700EB48J"
+    };
+  
+    
+  
+  //Declare a variable
+  firebase.initializeApp(firebaseConfig);
+  
+  const database = firebase.database();
+  
+ 
